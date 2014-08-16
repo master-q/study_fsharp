424a19c525d4d35bae546e99cbef798370346217
Kiwamu Okabe <kiwamu@debian.or.jp>
2014-08-16 13:47:03 +0900

    Simple read file / Add FParsec dll

--- music-box/music-box/Program.fs
+++ music-box/music-box/Program.fs
@@ -1,7 +1,9 @@
-﻿// Learn more about F# at http://fsharp.net
-// See the 'F# Tutorial' project for more help.
+﻿open System.IO
 
 [<EntryPoint>]
 let main argv = 
     printfn "%A" argv
+    let byte = File.ReadAllBytes(argv.[0])
+    let str = System.Text.ASCIIEncoding.Default.GetString byte
+    printf "%s" str
     0 // return an integer exit code
